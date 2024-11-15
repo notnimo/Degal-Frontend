e3169f0a7cf1c0e4906b4df3cbf26145a800dde1
AS <Arturo.Serpico@gmail.com>
2024-11-15 17:26:14 +0100

    Add files via upload
    new version

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import DegalLogo from './DegalLogo'
+import './App.css'
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+export default function App(props) {
+    useGSAP(() => {
+        //let tl = gsap.timeline({
+        //    scrollTrigger: {
+        //        scrub: 1,
+        //        trigger: '#logo',
+        //        start: "-200px top",
+        //        //endTrigger: 'body',
+        //        end: "+=5000"
+        //    }
+        //});
+
+        let scrollTrigger = {
+            scrub: 1,
+            trigger: '#logo',
+            start: "-100px top",
+            //endTrigger: 'body',
+            end: "+=5000"
+        }
+        gsap.to('#logo', {
+            scrollTrigger: scrollTrigger,
+            scale: 30
+        });
+        gsap.to('#logo>text, #logo-shadow>text', {
+            scrollTrigger: scrollTrigger,
+            y: -150,
+        });
+        gsap.to('#logo>path.bigV, #logo-shadow>path.bigV', {
+            scrollTrigger: scrollTrigger,
+            y: 150,
+        });
+        
+    });
+    return (
+        <>
+            <DegalLogo />
+            <div id="spacer" className="spacer" />
+        </>
+    );
+}
