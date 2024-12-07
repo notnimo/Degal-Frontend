30dd24ab5f19eaa7495382ff0f2d8131ee6ddbbb
__þami__ <damiano.livoni@gmail.com>
2024-12-07 22:14:24 +0000

    team-members-page-progress

--- src/teamMembers.jsx
+++ src/teamMembers.jsx
@@ -9,36 +9,50 @@ gsap.registerPlugin(ScrollTrigger);
 export default function TeamMembers(){
     return(
         <div id="teamMembersPage-wrap">
-            <h1>TEAM MEMBERS</h1>
-            <div id="team-members-wrap">
-                <div className="single-team-member">
-                    {/*<img src="" alt="" />*/}
-                    <h3>GABRIELE DI GIORGIO</h3>
-                </div>
-                <div className="single-team-member">
-                    {/*<img src="" alt="" />*/}
-                    <h3>DAMIANO LIVONI</h3>
-                </div>
-                <div className="single-team-member">
-                    {/*<img src="" alt="" />*/}
-                    <h3>ARTURO SERPICO</h3>
-                </div>
-                <div className="single-team-member">
-                    {/*<img src="" alt="" />*/}
-                    <h3>LEONARDO ZUCCHINI</h3>
-                </div>
-                <div className="single-team-member">
-                    {/*<img src="" alt="" />*/}
-                    <h3>ADRIANO ROGGERO</h3>
-                </div>
-                <div className="single-team-member">
-                    {/*<img src="" alt="" />*/}
-                    <h3>EMMA BONELLI</h3>
-                </div>
-                <div className="single-team-member">
-                    {/*<img src="" alt="" />*/}
-                    <h3>LORENZO TESSITORE</h3>
-                </div>
+            <div className="single-team-member-andheader">
+                <h1>TEAM MEMBERS</h1>
+            </div>
+            <div className="single-team-member-andHeader">
+                <img href="https://via.placeholder.com/150" alt="placeholder" />
+                <h3>GABRIELE DI GIORGIO</h3>
+                <h4>Team Manager</h4>
+                <p></p>
+            </div>
+            <div className="single-team-member-andHeader">
+                <img href="https://via.placeholder.com/150" alt="placeholder" />
+                <h3>DAMIANO LIVONI</h3>
+                <h4>Design Engineer</h4>
+                <p></p>
+            </div>
+            <div className="single-team-member-andHeader">
+                <img href="https://via.placeholder.com/150" alt="placeholder" />
+                <h3>ARTURO SERPICO</h3>
+                <h4>Design Engineer</h4>
+                <p></p>
+            </div>
+            <div className="single-team-member-andHeader">
+                <img href="https://via.placeholder.com/150" alt="placeholder" />
+                <h3>LEONARDO ZUCCHINI</h3>
+                <h4>Manufactoring Engineer</h4>
+                <p></p>
+            </div>
+            <div className="single-team-member-andHeader">
+                <img href="https://via.placeholder.com/150" alt="placeholder" />
+                <h3>ADRIANO ROGGERO</h3>
+                <h4>Graphic Designer</h4>
+                <p></p>
+            </div>
+            <div className="single-team-member-andHeader">
+                <img href="https://via.placeholder.com/150" alt="placeholder" />
+                <h3>EMMA BONELLI</h3>
+                <h4>Resource Manager</h4>
+                <p></p>
+            </div>
+            <div className="single-team-member-andHeader">
+                <img href="https://via.placeholder.com/150" alt="placeholder" />
+                <h3>LORENZO TESSITORE</h3>
+                <h4>Resource Manager</h4>
+                <p></p>
             </div>
         </div>
     );
