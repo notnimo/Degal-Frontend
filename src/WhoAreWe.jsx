2c0f202b2f0f5197671667af3694f9e0ca766950
__þami__ <damiano.livoni@gmail.com>
2025-02-08 19:02:19 +0000

    initialized second decorations

--- src/WhoAreWe.jsx
+++ src/WhoAreWe.jsx
@@ -1,79 +1,93 @@
-import React from "react"
-import gsap from 'gsap'
-import { useGSAP } from '@gsap/react'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import './WhoAreWe.css'
+import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import "./WhoAreWe.css";
 
 gsap.registerPlugin(ScrollTrigger);
 
-export default function WhoAreWe() 
-{
-    useGSAP(() => {
-        gsap.from('#whoAreWePage-content', {
-            scrollTrigger: {
-                start: 'top 40%',
-                endTrigger: '#whoAreWePage-wrap',
-                end: 'bottom bottom',
-                scrub: 1,
-                trigger: '#whoAreWePage-content',
-                pin: true
-            },
-            opacity: 0
-        });
-        
-        gsap.from('#whoAreWePage-content>div.circular-whoAreWePage-decorations>div' , {
-            scrollTrigger: {
-                start: 'top 40%',
-                endTrigger: '#whoAreWePage-wrap',
-                end: 'bottom bottom',
-                scrub: 1,
-                trigger: '#whoAreWePage-content',
-            },
-            left: -250
-        });
+export default function WhoAreWe() {
+	useGSAP(() => {
+		gsap.from("#whoAreWePage-content", {
+			scrollTrigger: {
+				start: "top 40%",
+				endTrigger: "#whoAreWePage-wrap",
+				end: "bottom bottom",
+				scrub: 1,
+				trigger: "#whoAreWePage-content",
+				pin: true,
+			},
+			opacity: 0,
+		});
 
-        let elements = document.querySelectorAll('#whoAreWePage-content>main>div');
+		gsap.from(
+			"#whoAreWePage-content>div.circular-whoAreWePage-decorations-left>div",
+			{
+				scrollTrigger: {
+					start: "top 40%",
+					endTrigger: "#whoAreWePage-wrap",
+					end: "bottom bottom",
+					scrub: 1,
+					trigger: "#whoAreWePage-content",
+				},
+				left: -250,
+			}
+		);
 
-        elements.forEach((element, index) => {
-            gsap.from(element, {
-                scrollTrigger: {
-                    start: 'top 40%',
-                    endTrigger: '#whoAreWePage-wrap',
-                    end: 'bottom bottom',
-                    scrub: 1,
-                    trigger: '#whoAreWePage-content',
-                },
-                translateX: index%2 ? -(200+index*20) : (200+index*20)
-            });
-        });
-    });
+		let elements = document.querySelectorAll(
+			"#whoAreWePage-content>main>div"
+		);
 
-    return (
-        <>
-            <div style={{height: 2000}}></div>
-            <div id="whoAreWePage-wrap">
-                <div id="whoAreWePage-content">
-                    <h1>Who are we?</h1>
-                    <main>
-                        <div>hello</div>
-                        <div>hello</div>
-                        <div>hello</div>
-                        <div>hello</div>
-                        <div>hello</div>
-                        <div>hello</div>
-                    </main>
-                    <div className="circular-whoAreWePage-decorations">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+		elements.forEach((element, index) => {
+			gsap.from(element, {
+				scrollTrigger: {
+					start: "top 40%",
+					endTrigger: "#whoAreWePage-wrap",
+					end: "bottom bottom",
+					scrub: 1,
+					trigger: "#whoAreWePage-content",
+				},
+				translateX: index % 2 ? -(200 + index * 20) : 200 + index * 20,
+			});
+		});
+	});
+
+	return (
+		<>
+			<div style={{ height: 2000 }}></div>
+			<div id="whoAreWePage-wrap">
+				<div id="whoAreWePage-content">
+					<h1>Who are we?</h1>
+					<main>
+						<div>hello</div>
+						<div>hello</div>
+						<div>hello</div>
+						<div>hello</div>
+						<div>hello</div>
+						<div>hello</div>
+					</main>
+					<div className="circular-whoAreWePage-decorations-left">
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+					<div className="circular-whoAreWePage-decorations-right">
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
