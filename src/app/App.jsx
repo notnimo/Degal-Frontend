f74935985ad0837fdb7cad1986ea46bcc1b58fed
__þami__ <damiano.livoni@gmail.com>
2025-03-11 20:20:12 +0000

    reorganized file structure

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import DegalLogo from "./DegalLogo";
-import ScrollSuggestion from "./scrollSuggestion";
-import WhoAreWe from "./WhoAreWe";
-import SponsorDisplay from "./sponsorPage";
-import TeamMembers from "./teamMembers";
+import DegalLogo from "../degal_logo/DegalLogo";
+import ScrollSuggestion from "../scroll_suggestion/scrollSuggestion";
+import WhoAreWe from "../who_are_we/WhoAreWe";
+import SponsorDisplay from "../sponsor_display/sponsorPage";
+import TeamMembers from "../team_members/teamMembers";
 import "./App.css";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
