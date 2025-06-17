cb5e0aa2cab16ec2d033fd05cf50d5077c15c5e0
__þami__ <damiano.livoni@gmail.com>
2025-06-17 09:15:43 +0000

    TM cards adjusted

--- src/team_members/teamMembers.jsx
+++ src/team_members/teamMembers.jsx
@@ -16,156 +16,114 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default function TeamMembers() {
 	const containerRef = useRef(null);
+	const cardsRef = useRef([]);
 
 	useGSAP(() => {
-		let TMCards = document.querySelectorAll(
-			".single-team-member-andHeader"
+		const container = containerRef.current;
+		const cards = cardsRef.current;
+
+		// Calculate total width of all cards
+		const totalCardsWidth = cards.reduce(
+			(acc, card) => acc + (card?.offsetWidth || 0),
+			0
 		);
 
-		gsap.to(containerRef.current, {
+		// Set the end value based on the total width of all cards minus the container width
+		const scrollEnd = totalCardsWidth - container.offsetWidth + 360; // 360px buffer
+
+		gsap.to(cards, {
+			x: () => `-${scrollEnd}px`,
+			ease: "none",
 			scrollTrigger: {
-				trigger: containerRef.current,
+				trigger: container,
 				start: "top top",
-				end: () => "+=" + containerRef.current.offsetWidth + "+950",
+				end: `+=${scrollEnd}`,
 				pin: true,
 				scrub: 1,
 			},
 		});
-
-		TMCards.forEach((element, index) => {
-			gsap.to(element, {
-				x: () => "-" + containerRef.current.offsetWidth + "-5000",
-				scrollTrigger: {
-					trigger: containerRef.current,
-					start: "top top",
-					end: () => "+=" + containerRef.current.offsetWidth + "+950",
-					scrub: 1,
-				},
-			});
-		});
-	});
+	}, []);
 
 	return (
-		<div id="teamMembersPage-wrap" ref={containerRef}>
+		<div
+			id="teamMembersPage-wrap"
+			ref={containerRef}
+			style={{ overflow: "hidden" }}
+		>
 			<div className="space" id="initTrigger"></div>
-			<div className="single-team-member-andHeader">
-				<h1>TEAM MEMBERS</h1>
-			</div>
-			<div className="single-team-member-andHeader">
-				<div className="TM-cont">
-					<div className="TM-image-cont">
-						<img src={digio} />
+			{[
+				{
+					name: "TEAM MEMBERS",
+					isHeader: true,
+				},
+				{
+					img: digio,
+					name: "GABRIELE DI GIORGIO",
+					role: "Team Manager",
+					desc: "I am the one who coordinates the team. With ability I was able to understand from the very beginning what had to be done to achieve the established goals and to find efficient alternatives in difficult moments. My work is essential for the good functioning of the team's activities",
+				},
+				{
+					img: damiano,
+					name: "DAMIANO LIVONI",
+					role: "Design Engineer",
+					desc: "I started with the only role of running the simulation to improve the aerodynamics of the car, but I also thought that the team needed a website for marketing purposes so i learned web development and coded this website. I sometimes help out the other team members with their tasks if needed.",
+				},
+				{
+					img: arturo,
+					name: "ARTURO SERPICO",
+					role: "Design Engineer",
+					desc: "Despite being only 14 years old, I am extremely passionate about the world of science, especially mathematics and computer science. I quickly learned how to model with CAD software and designed an aerodynamic car. I am an active part of the team's success",
+				},
+				{
+					img: leonardo,
+					name: "LEONARDO ZUCCHINI",
+					role: "Manufacturing Engineer",
+					desc: "My role consists of hand finishing the final printed model and designing its mechanisms: thanks to my manufacturing skills it was possible to show up at the race with cars that were not only fast but also functional. My presence is also of great importance for the performance of the car, as I design the wheels and the wing attachment systems",
+				},
+				{
+					img: adriano,
+					name: "ADRIANO ROGGERO",
+					role: "Graphic Designer",
+					desc: "If it wasn't for me, the team wouldn't have a logo, official colors and graphic elements. My contribute is of vital importance, as I actively work to brand the team, creating eye-catching images for the public",
+				},
+				{
+					img: emma,
+					name: "EMMA BONELLI",
+					role: "Resource Manager",
+					desc: "I'm the only girl in the team, and I carefully and skillfully analyze the latest trends on social media to understand what content needs to be published. I therefore carry out a fundamental part of the work in making the team's accounts popular, following the brand identity established by all of us.",
+				},
+				{
+					img: tex,
+					name: "LORENZO TESSITORE",
+					role: "Resource Manager",
+					desc: "Winning: this is the goal, and to do so it's necessary an adequate marketing strategy. I am the mind behind the team's commercial choices, if the team had results not only in the engineering field it owes me a lot for this.",
+				},
+			].map((member, idx) =>
+				member.isHeader ? (
+					<div
+						className="single-team-member-andHeader"
+						key={idx}
+						ref={(el) => (cardsRef.current[idx] = el)}
+					>
+						<h1>{member.name}</h1>
 					</div>
-					<h3>GABRIELE DI GIORGIO</h3>
-					<h4>Team Manager</h4>
-					<p className="TM-paragraph">
-						I am the one who coordinates the team. With ability I
-						was able to understand from the very beginning what had
-						to be done to achieve the established goals and to find
-						efficient alternatives in difficult moments. My work is
-						essential for the good functioning of the team's
-						activities
-					</p>
-				</div>
-			</div>
-			<div className="single-team-member-andHeader">
-				<div className="TM-cont">
-					<div className="TM-image-cont">
-						<img src={damiano} />
+				) : (
+					<div
+						className="single-team-member-andHeader"
+						key={idx}
+						ref={(el) => (cardsRef.current[idx] = el)}
+					>
+						<div className="TM-cont">
+							<div className="TM-image-cont">
+								<img src={member.img} alt={member.name} />
+							</div>
+							<h3>{member.name}</h3>
+							<h4>{member.role}</h4>
+							<p className="TM-paragraph">{member.desc}</p>
+						</div>
 					</div>
-					<h3>DAMIANO LIVONI</h3>
-					<h4>Design Engineer</h4>
-					<p className="TM-paragraph">
-						I started with the only role of running the simulation
-						to improve the aerodynamics of the car, but I also
-						thought that the team needed a website for marketing
-						purposes so i learned web development and coded this
-						website. I sometimes help out the other team members
-						with their tasks if needed.
-					</p>
-				</div>
-			</div>
-			<div className="single-team-member-andHeader">
-				<div className="TM-cont">
-					<div className="TM-image-cont">
-						<img src={arturo} />
-					</div>
-					<h3>ARTURO SERPICO</h3>
-					<h4>Design Engineer</h4>
-					<p className="TM-paragraph">
-						Despite being only 14 years old, I am extremely
-						passionate about the world of science, especially
-						mathematics and computer science. I quickly learned how
-						to model with CAD software and designed an aerodynamic
-						car. I am an active part of the team's success
-					</p>
-				</div>
-			</div>
-			<div className="single-team-member-andHeader">
-				<div className="TM-cont">
-					<div className="TM-image-cont">
-						<img src={leonardo} />
-					</div>
-					<h3>LEONARDO ZUCCHINI</h3>
-					<h4>Manufacturing Engineer</h4>
-					<p className="TM-paragraph">
-						My role consists of hand finishing the final printed
-						model and designing its mechanisms: thanks to my
-						manufacturing skills it was possible to show up at the
-						race with cars that were not only fast but also
-						functional. My presence is also of great importance for
-						the performance of the car, as I design the wheels and
-						the wing attachment systems
-					</p>
-				</div>
-			</div>
-			<div className="single-team-member-andHeader">
-				<div className="TM-cont">
-					<div className="TM-image-cont">
-						<img src={adriano} />
-					</div>
-					<h3>ADRIANO ROGGERO</h3>
-					<h4>Graphic Designer</h4>
-					<p className="TM-paragraph">
-						If it wasn't for me, the team wouldn't have a logo,
-						official colors and graphic elements. My contribute is
-						of vital importance, as I actively work to brand the
-						team, creating eye-catching images for the public
-					</p>
-				</div>
-			</div>
-			<div className="single-team-member-andHeader">
-				<div className="TM-cont">
-					<div className="TM-image-cont">
-						<img src={emma} />
-					</div>
-					<h3>EMMA BONELLI</h3>
-					<h4>Resource Manager</h4>
-					<p className="TM-paragraph">
-						I'm the only girl in the team, and I carefully and
-						skillfully analyze the latest trends on social media to
-						understand what content needs to be published. I
-						therefore carry out a fundamental part of the work in
-						making the team's accounts popular, following the brand
-						identity established by all of us.
-					</p>
-				</div>
-			</div>
-			<div className="single-team-member-andHeader" id="endTGsap">
-				<div className="TM-cont">
-					<div className="TM-image-cont">
-						<img src={tex} />
-					</div>
-					<h3>LORENZO TESSITORE</h3>
-					<h4>Resource Manager</h4>
-					<p className="TM-paragraph">
-						Winning: this is the goal, and to do so it's necessary
-						an adequate marketing strategy. I am the mind behind the
-						team's commercial choices, if the team had results not
-						only in the engineering field it owes me a lot for this.
-					</p>
-				</div>
-			</div>
+				)
+			)}
 			<div className="space"></div>
 		</div>
 	);
